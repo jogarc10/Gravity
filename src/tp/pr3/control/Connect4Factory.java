6486d3aa1a581db0481ffafbce9e5f8cc8a3296c
Jorge <silversoldier9@hotmail.com>
2015-02-23 23:51:41 +0100

    Writing Connect 4 Factory. [Unfinished]
    Ok. So, how can I create an object from a public interface? LOL

--- src/tp/pr3/control/Connect4Factory.java
+++ src/tp/pr3/control/Connect4Factory.java
@@ -2,34 +2,29 @@ package tp.pr3.control;
 
 import java.util.Scanner;
 
+import tp.pr3.logic.Connect4Move;
+import tp.pr3.logic.Connect4Rules;
 import tp.pr3.logic.Counter;
 import tp.pr3.logic.GameRules;
 import tp.pr3.logic.Move;
 
 public class Connect4Factory implements GameTypeFactory {
 
-	@Override
-	public Player createHumanPlayerAtConsole(Scanner in) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Move createMove(int col, int row, Counter colour) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Player createRandomPlayer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public GameRules createRules() {
-		// TODO Auto-generated method stub
+		return new Connect4Rules();
+	}
+
+	public Move createMove(int col, int row, Counter colour) {
+		return new Connect4Move(col, colour);
+	}
+
+	public Player createHumanPlayerAtConsole(Scanner in) {
+		/** What the fuck??? Cómo voy a crear un objecto de una interface?  */
 		return null;
 	}
-	 
+  
+	public Player createRandomPlayer() {
+		return new RandomConnect4Player();
+	}
+
 } 
