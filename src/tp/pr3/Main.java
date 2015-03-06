3274c538e89dca24a8a0436f6ad4ad9ffe81333d
pegartillo <pegarc03@ucm.es>
2015-03-06 14:34:46 +0100

    Ahora creo que tiene que ir

--- src/tp/pr3/Main.java
+++ src/tp/pr3/Main.java
@@ -20,112 +20,97 @@ public class Main {
 		Game game;
 		Scanner in = new Scanner(System.in);;
 		Controller controller;
-		boolean valid = false;
-		String optionString = "", lowerCaseStr;
 		
-		GameRules gameRules = null;
-		GameTypeFactory f = null;
+		GameRules gameRules = new Connect4Rules();
+		GameTypeFactory f = new Connect4Factory();
 		
-		
-		while (!valid){
-			System.out.println("%> java [details omitted] tp.pr3.Main");
 			
-			optionString = in.nextLine().toUpperCase();
-			lowerCaseStr = optionString.toLowerCase();
-			String[] words = optionString.split(" ");
-			
-			if (words.length == 1) {
-				if (words[0].equals("-H") || words[0].equals("--HELP")){
+			if (args.length == 1) {
+				if (args[0].equals("-h") || args[0].equals("--help")){
 					Resources.helpInit();
 				}
 				else{
-					System.err.println("Incorrect use: Unrecognized option: " + lowerCaseStr);
+					System.err.println("Incorrect use: Unrecognized option: " + args);
 					System.err.println("For more details, use -h|--help.");
 				}
 			}
-			else if (words.length == 2) {
-				if (words[0].equals("-G") || words[0].equals("-GAME")){
-					if (words[1].equals("C4")){
+			else if (args.length == 2) {
+				if (args[0].equals("-g") || args[0].equals("--game")){
+					if (args[1].equals("c4")){
 						gameRules = new Connect4Rules();
 						f = new Connect4Factory();
-						valid = true;
 					}
-					else if (words[1].equals("CO")){
+					else if (args[1].equals("co")){
 						gameRules = new ComplicaRules();
 						f = new ComplicaFactory();
-						valid = true;
 					}
-					else if (words[1].equals("GR")){
+					else if (args[1].equals("gr")){
 						gameRules = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
 						f = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-						valid = true;
 					}
 					else {
-						System.err.println("Incorrect use: game ’" + words[1].toLowerCase() + "’ incorrect.");
+						System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
 						System.err.println("For more details, use -h|--help.");
 					}
 				}
 				else {
-					System.err.println("Incorrect use: Unrecognized option: " + words[0].toLowerCase());
+					System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
 					System.err.println("For more details, use -h|--help.");
 				}
 			}
-			else if (words.length == 6){
-				if (words[0].equals("-G") || words[0].equals("--GAME")){
-					if (words[1].equals("GR")){
-						if (words[2].equals("-X") || words[2].equals("--DIMX")){
+			else if (args.length == 6){
+				if (args[0].equals("-g") || args[0].equals("--game")){
+					if (args[1].equals("gr")){
+						if (args[2].equals("-x") || args[2].equals("--dimX")){
 							try {
-								   Integer.parseInt(words[3]);
-								   if (words[4].equals("-Y") || words[4].equals("--DIMY")){
+								   Integer.parseInt(args[3]);
+								   if (args[4].equals("-y") || args[4].equals("--dimY")){
 									   try {
-										   Integer.parseInt(words[5]);
-										   Resources.setGravityDimX(Integer.parseInt(words[3]));
-										   Resources.setGravityDimY(Integer.parseInt(words[5]));
+										   Integer.parseInt(args[5]);
+										   Resources.setGravityDimX(Integer.parseInt(args[3]));
+										   Resources.setGravityDimY(Integer.parseInt(args[5]));
 										   gameRules = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
 										   f = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-										   valid = true;
 										   
 									   }
 									   catch(NumberFormatException e){
-										   System.err.println("Incorrect use: illegal arguments: " + words[2] + " " + words[3] + " " + words[4] + " " + words[5]);
+										   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
 										   System.err.println("For more details, use -h|--help.");
 									   }
 								   }
 								   else {
-									   System.err.println("Incorrect use: illegal arguments: " + words[2] + " " + words[3] + " " + words[4] + " " + words[5]);
+									   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
 									   System.err.println("For more details, use -h|--help.");
 								   }
 							}
 							catch(NumberFormatException e){
-								System.err.println("Incorrect use: illegal arguments: " + words[2] + " " + words[3] + " " + words[4] + " " + words[5]);
+								System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
 								System.err.println("For more details, use -h|--help.");
 							}
 						}
 						else {
-							System.err.println("Incorrect use: illegal arguments: " + words[2] + " " + words[3] + " " + words[4] + " " + words[5]);
+							System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
 							System.err.println("For more details, use -h|--help.");
 						}
 					}
-					else if(words[1].equals("C4") || words[1].equals("CO")){
-						System.err.println("Incorrect use: illegal arguments: " + words[2] + " " + words[3] + " " + words[4] + " " + words[5]);
+					else if(args[1].equals("c4") || args[1].equals("co")){
+						System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
 						System.err.println("For more details, use -h|--help.");
 					}
 					else{
-						System.err.println("Incorrect use: game ’" + words[1].toLowerCase() + "’ incorrect.");
+						System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
 						System.err.println("For more details, use -h|--help.");
 					}
 				}
 				else {
-					System.err.println("Incorrect use: Unrecognized option: " + words[0].toLowerCase());
+					System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
 					System.err.println("For more details, use -h|--help.");
 				}
 			}
 			else {
-				System.err.println("Incorrect use: Unrecognized option: " + words[0].toLowerCase());
+				System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
 				System.err.println("For more details, use -h|--help.");
 			}
-			
-		}
 		
 		game = new Game(gameRules);
 		
